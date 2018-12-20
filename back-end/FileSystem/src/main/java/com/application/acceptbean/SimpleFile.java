509bd9a600e52e8bdec8647932341a555f625596
0-Avalon-0 <32134061+0-Avalon-0@users.noreply.github.com>
2018-12-20 19:30:48 +0800

    提交了匹配前端的补丁

--- back-end/FileSystem/src/main/java/com/application/acceptbean/SimpleFile.java
+++ back-end/FileSystem/src/main/java/com/application/acceptbean/SimpleFile.java
@@ -1,6 +1,9 @@
 package com.application.acceptbean;
 
 public class SimpleFile {
+	private int pid = 0;
+	private String file_text = "";
+	private String path = "";
 	private String file_fname;
 	private int file_property;
 	public void setfile_fname(String name) {
@@ -9,7 +12,26 @@ public class SimpleFile {
 	public void setfile_property(int property) {
 		this.file_property = property;
 	}
+	public void setpid(int pid) {
+		this.pid = pid;
+	}
+	public void setpath(String path) {
+		this.path = path;
+	}
+	public void setfile_text(String text) {
+		this.file_text  = text;
+	}
 	
+	
+	public int getpid() {
+		return this.pid;
+	}
+	public String getpath() {
+		return this.path;
+	}
+	public String getfile_text() {
+		return this.file_text;
+	}
 	public String getfile_fname() {
 		return this.file_fname;
 	}
